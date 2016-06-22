3c187704124ad366a8b0a007965cde22c4f650c5
Albert Besuhoff <albert.besuhoff@gmail.com>
2016-06-23 01:32:37 +0300

    Add payment methods

--- index.php
+++ index.php
@@ -3,9 +3,20 @@ require __DIR__ . '/vendor/autoload.php';
  
 // Your App
 $app = new Bullet\App();
-$app->path('/', function($request) {
-    return "Hello World!";
+$app->path('/payment-methods', function($request) {
+    return [
+        [
+            'id' => 1,
+            'name' => "Укрсиб",
+            'currency' => 'UAH'
+        ],
+        [
+            'id' => 2,
+            'name' => "Наличные",
+            'currency' => 'UAH'
+        ]
+    ];
 });
- 
+
 // Run the app! (takes $method, $url or Bullet\Request object)
 echo $app->run(new Bullet\Request());
