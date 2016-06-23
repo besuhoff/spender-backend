872e50e2cb42c51389d91a4db1ec0acac2f497f6
Albert Besuhoff <albert.besuhoff@gmail.com>
2016-06-23 18:56:13 +0300

    Restrict all entities per user

--- index.php
+++ index.php
@@ -4,19 +4,27 @@ require_once __DIR__ . '/spender/generated-conf/config.php';
 
 
 header('Access-Control-Allow-Origin: http://spender.pereborstudio.dev:8081');
-header('Access-Control-Allow-Headers: X-Auth-Token');
+header('Access-Control-Allow-Headers: X-Auth-Token, Content-Type');
 define('GAPI_CLIENT_ID', '843225840486-ilkj47kggue9tvh6ajfvvog45mertgfg.apps.googleusercontent.com');
 
 $gapiUserId = false;
 $token = isset($_SERVER['HTTP_X_AUTH_TOKEN']) ? $_SERVER['HTTP_X_AUTH_TOKEN'] : '';
 
 $app = new Bullet\App();
+$user = false;
 
 if ($token) {
     $gapiResponse = json_decode(file_get_contents('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' . urlencode($token)));
 
     if ($gapiResponse->aud === GAPI_CLIENT_ID) {
         $gapiUserId = $gapiResponse->sub;
+        $user = \Base\UserQuery::create()->findOneByGapiUserId($gapiUserId);
+
+        if (!$user) {
+            $user = new User();
+            $user->setGapiUserId($gapiUserId);
+            $user->save();
+        }
     }
 }
 
@@ -25,17 +33,30 @@ if (!$gapiUserId && $_SERVER['REQUEST_METHOD'] !== 'OPTIONS') {
     exit();
 };
 
-$app->path('/payment-methods', function($request) use($app) {
-    $app->get(function($request) {
-        $paymentMethods = \Base\PaymentMethodQuery::create()->orderByName()->find();
+$app->path('/payment-methods', function($request) use($app, $user) {
+    $app->get(function($request) use($app, $user) {
+        $paymentMethods = \Base\PaymentMethodQuery::create()->orderByName()->findByUserId($user->getId());
         return $paymentMethods->toArray();
     });
 });
 
-$app->path('/categories', function($request) use($app) {
-    $app->get(function($request) {
-        $categories = \Base\CategoryQuery::create()->orderByName()->find();
+$app->path('/categories', function($request) use($app, $user) {
+    $app->get(function($request) use($app, $user) {
+        $categories = \Base\CategoryQuery::create()->orderByName()->findByUserId($user->getId());
         return $categories->toArray();
+    });
+});
+
+$app->path('/expenses', function($request) use($app, $user) {
+    $app->post(function($request) use($app, $user) {
+        $expense = new Expense();
+        $expense->setAmount($request->Amount);
+        $expense->setCategoryId($request->CategoryId);
+        $expense->setPaymentMethodId($request->PaymentMethodId);
+        $user->addExpense($expense);
+        $user->save();
+
+        return $expense->toArray();
     });
 });
 
